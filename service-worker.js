56eaf7e979c347510224f8918235f1f5d1e991de
rabindramahato97-sudo <rabindramahato97@gmail.com>
2026-09-07 18:12:47 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="learn-play-v21";
+const CACHE="learn-play-v22";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>{
  self.skipWaiting();
