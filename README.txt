26a6a525508682f9e3582e37e2d275d6003a5e82
rabindramahato97-sudo <rabindramahato97@gmail.com>
2026-09-07 11:09:06 +0530

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,3 +1,6 @@
-Learn & Play V2
-Features: child profile, Nursery-Class 5, Maths/English/Hindi/EVS/GK, 3 lives, stars, coins, rewards and level unlocking.
-Upload index.html, manifest.json, service-worker.js and icon.svg to the root of your GitHub Pages repository.
+Learn & Play PWA V2
+Features: child name, Nursery-Class 5 selection, Maths/English/Hindi/EVS/GK, 3 lives, stars, coins, quiz levels and rewards.
+Upload index.html, manifest.json, service-worker.js and icon.svg to the GitHub Pages repository root.
+
+
+Version 3: Start Learning now saves the child profile and immediately opens the Maths quiz. Cache version updated to v3.
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='learn-play-v2';const A=['./','./index.html','./manifest.json','./service-worker.js','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let c=res.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return res}).catch(()=>caches.match('./index.html')))));
+const CACHE="learn-play-v3";
+const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
